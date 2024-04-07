074504e0844327bfe6e64904eda88c90ba15a49a
komalvyas366 <komalvyas366@gmail.com>
2024-04-07 19:50:52 +0300

    Lab 8 completed

---
+++ models/contactModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: Number, required: true },
+  address: { type: String },
+});
+
+
+module.exports = mongoose.model('Contact', contactSchema);
